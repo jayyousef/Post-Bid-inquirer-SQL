78b6206a44cb8146abc4cb0f42ed581a68894911
Jay Yousef <jay.yousef@gmail.com>
2021-04-17 15:02:17 -0500

    pushing mostly developed but not full finished application for further development. TODO - write if->then which handles the userBid being lower or higher than the current highest bid, and then update the database. Future functionality could include a 'days reamining' section which would count down every day until the item ends ,then update the original poster their item is sold and what the ending bid price was

--- greatBaySchema.sql
+++ greatBaySchema.sql
@@ -7,7 +7,12 @@ CREATE TABLE auctions(
   id INT NOT NULL AUTO_INCREMENT,
   item_name VARCHAR(100) NOT NULL,
   category VARCHAR(45) NOT NULL,
+conditions VARCHAR(45) NOT NULL,
   starting_bid INT default 0,
   highest_bid INT default 0,
   PRIMARY KEY (id)
 );
+
+
+INSERT INTO auctions (item_name, category, conditions ,starting_bid)
+VALUES ("test-item", "Technology","New", 0),("test-item-2", "Auto","Like-New", 50),("test-item-3", "Collectibles","Used", 100),("test-item-4", "Household","Should Burn", 1889)
